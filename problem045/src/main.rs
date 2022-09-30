81a8aa37ba9e2dbdb989ffba945bde46d3a6bd6a
jasp <jackspagnoli@gmail.com>
2022-09-30 16:14:00 +0100

    Solved problem 045

--- problem045/src/main.rs
+++ problem045/src/main.rs
@@ -1,3 +1,17 @@
+use num_integer::Roots;
 fn main() {
-    println!("Hello, world!");
+    let mut n:u128 = 286;
+    while !tri_pent_hex(n){
+        n+=1;
+    }
+    println!("{}", (n*(n+1))/2);
+}
+fn tri_pent_hex(n:u128)->bool{
+    let sqrt:u128 = (1+(12*n*(n+1))).sqrt();
+    if sqrt * sqrt != (1+(12*n*(n+1))) { return false; }
+    let sqrt:u128 = (1+(4*n*(n+1))).sqrt();
+    if sqrt * sqrt != (1+(4*n*(n+1))) { return false; }
+    if (1+((1+(12*n*(n+1))).sqrt())) % 6 != 0 { return false; }
+    if (1+(1+(4*n*(n+1))).sqrt()) % 4 != 0 { return false; }
+    return true;
 }
