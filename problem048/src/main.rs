c390343cc2faf073d6f19cecfa3993d7a76b6e65
jasp <jackspagnoli@gmail.com>
2022-10-06 15:19:32 +0100

    Solved problem 048

--- problem048/src/main.rs
+++ problem048/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sum: u128 = 0;
+    for i in 1..1001 as u128 {
+        let mut product: u128 = 1;
+        for _ in 0..i {
+            product *= i;
+            product %= 10_000_000_000;
+        }
+        sum += product;
+        sum %= 10_000_000_000;
+    }
+    println!("Sum: {}", sum);
 }
