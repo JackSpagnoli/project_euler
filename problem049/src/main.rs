432adc0a5e0ab202b3bf215386dd3f870b30f13a
jasp <jackspagnoli@gmail.com>
2022-10-06 16:22:18 +0100

    Solved problem049

--- problem049/src/main.rs
+++ problem049/src/main.rs
@@ -1,3 +1,48 @@
 fn main() {
-    println!("Hello, world!");
+    let mut primes: Vec<u64> = vec![2, 3, 5, 7];
+    generate_primes(&mut primes, 9_973);
+    primes.reverse();
+    primes.truncate(1061);
+    primes.reverse();
+    for n in &primes {
+        for k in 2..((9973 - n) / 2) {
+            if primes.contains(&(n + k)) {
+                if primes.contains(&(n + k + k)) {
+                    if permutation(n, &(n + k)) {
+                        if permutation(n, &(n + k + k)) {
+                            if permutation(&(n + k), &(n + k + k)) {
+                                println!("{}, {}, {}. k: {}", n, n + k, n + k + k, k);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn generate_primes(primes: &mut Vec<u64>, n: u64) {
+    let mut temp_n = primes[primes.len() - 1] + 2;
+    while primes[primes.len() - 1] < n {
+        if primes.iter().map(|x| temp_n % x == 0).filter(|x| *x).collect::<Vec<bool>>().len() == 0 { primes.push(temp_n); }
+        temp_n += 2;
+    }
+}
+
+fn permutation(a: &u64, b: &u64) -> bool {
+    let digits_a: [u64; 4] = [a % 10, (a / 10) % 10, (a / 100) % 10, (a / 1000) % 10];
+
+    if !digits_a.contains(&(b % 10)) { return false; }
+    if !digits_a.contains(&((b / 10) % 10)) { return false; }
+    if !digits_a.contains(&((b / 100) % 10)) { return false; }
+    if !digits_a.contains(&((b / 1000) % 10)) { return false; }
+
+    let digits_b: [u64; 4] = [b % 10, (b / 10) % 10, (b / 100) % 10, (b / 1000) % 10];
+
+    if !digits_b.contains(&(a % 10)) { return false; }
+    if !digits_b.contains(&((a / 10) % 10)) { return false; }
+    if !digits_b.contains(&((a / 100) % 10)) { return false; }
+    if !digits_b.contains(&((a / 1000) % 10)) { return false; }
+
+    return true;
 }
