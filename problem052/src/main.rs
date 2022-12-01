5bedb6ea9830912140d056604ac4eb99c890ca63
jasp <jackspagnoli@gmail.com>
2022-12-01 15:17:54 +0000

    solves problem052

--- problem052/src/main.rs
+++ problem052/src/main.rs
@@ -1,3 +1,25 @@
+use number_utils::get_digits;
+
 fn main() {
-    println!("Hello, world!");
+    let mut x: u128 = 1;
+    loop {
+        let x_digits = get_digits(x);
+
+        let mut i = 2;
+        let mut invalid = false;
+        while i <= 6 && !invalid {
+            let multiple_digits = get_digits(i * x);
+            if x_digits != multiple_digits {
+                invalid = true;
+            }
+            i+=1;
+        }
+
+        if !invalid {
+            println!("{x}");
+            return;
+        }
+
+        x += 1;
+    }
 }
