8a721118f3e45c2662a99dc61c45fa7c4ae5564d
Jack Spagnoli <jack@spagnoli.co.uk>
2023-08-30 18:06:07 +0100

    overhauls problem running

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,34 @@
 #![feature(iter_next_chunk)]
 
+pub enum Whole128 {
+    Signed(i128),
+    Unsigned(u128),
+}
+
+#[macro_export()]
+macro_rules! define_problem {
+    ($module:ident, $expected:expr) => {{
+        let problem_number = &stringify!($module)[7..10];
+        use $module::ans as ans_func;
+        let expected_value = $expected;
+        if expected_value < 0 {
+            let return_function: &dyn Fn() -> Whole128 = &|| Whole128::Signed(ans_func() as i128);
+            (
+                return_function,
+                Whole128::Signed(expected_value as i128),
+                problem_number,
+            )
+        } else {
+            let return_function: &dyn Fn() -> Whole128 = &|| Whole128::Unsigned(ans_func() as u128);
+            (
+                return_function,
+                Whole128::Unsigned(expected_value as u128),
+                problem_number,
+            )
+        }
+    }};
+}
+
 pub mod number_utils;
 pub mod primes;
 
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,58 @@
+use std::time::Instant;
+
 use project_euler::*;
 
 fn main() {
-    assert_eq!(problem027::ans(), -59231);
-    assert_eq!(problem029::ans(), 9183);
-    assert_eq!(problem030::ans(), 443839);
-    assert_eq!(problem031::ans(), 73682);
-    // assert_eq!(problem033::ans(), 100);
-    assert_eq!(problem034::ans(), 40730);
-    assert_eq!(problem035::ans(), 55);
-    assert_eq!(problem036::ans(), 872187);
-    assert_eq!(problem037::ans(), 748317);
-    assert_eq!(problem038::ans(), 932718654);
-    assert_eq!(problem039::ans(), 840);
-    assert_eq!(problem040::ans(), 210);
-    assert_eq!(problem041::ans(), 7652413);
-    assert_eq!(problem042::ans(), 162);
-    assert_eq!(problem043::ans(), 16695334890);
-    assert_eq!(problem044::ans(), 5482660);
-    assert_eq!(problem045::ans(), 1533776805);
-    assert_eq!(problem046::ans(), 5777);
-    assert_eq!(problem047::ans(), 134043);
-    assert_eq!(problem048::ans(), 9110846700);
-    assert_eq!(problem049::ans(), 296962999629);
-    assert_eq!(problem050::ans(), 997651);
-    // assert_eq!(problem051::ans(), 121313);
-    assert_eq!(problem052::ans(), 142857);
-    assert_eq!(problem053::ans(), 4075);
-    assert_eq!(problem054::ans(), 376);
-    assert_eq!(problem055::ans(), 249);
-    assert_eq!(problem056::ans(), 972);
-    assert_eq!(problem057::ans(), 153);
+    let problems: Vec<(&dyn Fn() -> Whole128, Whole128, &str)> = vec![
+        define_problem!(problem027, -59231),
+        define_problem!(problem029, 9183),
+        define_problem!(problem030, 443839),
+        define_problem!(problem031, 73682),
+        // define_problem!(problem033, 100),
+        define_problem!(problem034, 40730),
+        define_problem!(problem035, 55),
+        define_problem!(problem036, 872187),
+        define_problem!(problem037, 748317),
+        define_problem!(problem038, 932718654),
+        define_problem!(problem039, 840),
+        define_problem!(problem040, 210),
+        define_problem!(problem041, 7652413),
+        define_problem!(problem042, 162),
+        define_problem!(problem043, 16695334890u128),
+        define_problem!(problem044, 5482660),
+        define_problem!(problem045, 1533776805),
+        define_problem!(problem046, 5777),
+        define_problem!(problem047, 134043),
+        define_problem!(problem048, 9110846700u128),
+        define_problem!(problem049, 296962999629u128),
+        define_problem!(problem050, 997651),
+        // define_problem!(problem051, 121313),
+        define_problem!(problem052, 142857),
+        define_problem!(problem053, 4075),
+        define_problem!(problem054, 376),
+        define_problem!(problem055, 249),
+        define_problem!(problem056, 972),
+        define_problem!(problem057, 153),
+    ];
+
+    problems.iter().for_each(check_problem)
+}
+
+fn check_problem(problem: &(&dyn Fn() -> Whole128, Whole128, &str)) {
+    let problem_function = problem.0;
+    let expected = &problem.1;
+    let problem_name = problem.2;
+
+    let now = Instant::now();
+
+    let actual_result = problem_function();
+
+    let duration = now.elapsed().as_secs_f32();
+    println!("Problem {problem_name} took {duration} seconds");
+
+    match (actual_result, expected) {
+        (Whole128::Signed(actual), Whole128::Signed(expected)) => assert_eq!(actual, *expected),
+        (Whole128::Unsigned(actual), Whole128::Unsigned(expected)) => assert_eq!(actual, *expected),
+        _ => panic!("Expected return type does not match actual return type"),
+    }
 }
