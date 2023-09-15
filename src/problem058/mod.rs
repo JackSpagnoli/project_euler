3256509fc3ced8b84c5d1b8d3b673f0e03e05af6
Jack Spagnoli <jack@spagnoli.co.uk>
2023-09-15 15:59:23 +0100

    refactor

--- src/problem058/mod.rs
+++ src/problem058/mod.rs
@@ -2,9 +2,14 @@ use self::spiral_corner_prime_ratio::PrimeRatio;
 
 pub fn ans() -> u128 {
     PrimeRatio::default()
-        .find(|((numerator, denominator), _)| denominator >= &(10 * numerator))
+        .find(ratio_less_than_ten_percent)
         .unwrap()
         .1 as u128
+}
+
+fn ratio_less_than_ten_percent(ratio_output: &((u128, u128), usize)) -> bool{
+    let ((numerator, denominator), _) = ratio_output;
+    denominator >= &(10 * numerator)
 }
 
 mod spiral_corner_prime_ratio;
