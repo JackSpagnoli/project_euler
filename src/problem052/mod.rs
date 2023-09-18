9a0f6a70bf0085cc6a10520fb4c5f7c421260e12
Jack Spagnoli <jack@spagnoli.co.uk>
2023-09-18 14:14:11 +0100

    updates 052 to use new digit functions

--- src/problem052/mod.rs
+++ src/problem052/mod.rs
@@ -1,18 +1,18 @@
-use crate::number_utils::get_digits;
+use crate::number_utils::digit_dist::DigitDistribution;
 
 pub fn ans() -> u128 {
     let mut x: u128 = 1;
     loop {
-        let x_digits = get_digits(x);
+        let x_digits = DigitDistribution::from(x).get_distribution();
 
         let mut i = 2;
         let mut invalid = false;
         while i <= 6 && !invalid {
-            let multiple_digits = get_digits(i * x);
+            let multiple_digits = DigitDistribution::from(i * x).get_distribution();
             if x_digits != multiple_digits {
                 invalid = true;
             }
-            i+=1;
+            i += 1;
         }
 
         if !invalid {
