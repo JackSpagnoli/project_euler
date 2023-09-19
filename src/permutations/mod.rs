c1638ac442606aaa18f8b752539f441a23aac08c
Jack Spagnoli <jack@spagnoli.co.uk>
2023-09-19 17:00:22 +0100

    adds generic permuter

--- src/permutations/mod.rs
+++ src/permutations/mod.rs
@@ -1,15 +1,15 @@
 use num_integer::Integer;
 
-pub struct Permutations {
-    elements: Vec<usize>,
+pub struct Permutations<T: Clone> {
+    elements: Vec<T>,
     num_elements: usize,
     stack_state: Vec<usize>,
     stack_pointer: usize,
     first_iter: bool,
 }
 
-impl Iterator for Permutations {
-    type Item = Vec<usize>;
+impl<T: Clone> Iterator for Permutations<T> {
+    type Item = Vec<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.first_iter {
@@ -22,11 +22,12 @@ impl Iterator for Permutations {
             } else if self.stack_state[self.stack_pointer] >= self.stack_pointer {
                 self.stack_state[self.stack_pointer] = 0;
                 self.stack_pointer += 1;
-            }else{
+            } else {
                 if self.stack_pointer.is_even() {
                     self.elements.swap(0, self.stack_pointer)
                 } else {
-                    self.elements.swap(self.stack_state[self.stack_pointer], self.stack_pointer)
+                    self.elements
+                        .swap(self.stack_state[self.stack_pointer], self.stack_pointer)
                 }
                 self.stack_state[self.stack_pointer] += 1;
                 self.stack_pointer = 1;
@@ -37,11 +38,28 @@ impl Iterator for Permutations {
     }
 }
 
-impl From<usize> for Permutations {
+impl From<usize> for Permutations<usize> {
     fn from(value: usize) -> Self {
         let elements: Vec<usize> = (0..value).collect();
         let num_elements = elements.len();
         let stack_state = vec![0; value];
+        let stack_pointer = 1;
+        let first_iter = true;
+        Self {
+            elements,
+            num_elements,
+            stack_state,
+            stack_pointer,
+            first_iter,
+        }
+    }
+}
+
+impl<T: Clone> From<Vec<T>> for Permutations<T> {
+    fn from(value: Vec<T>) -> Self {
+        let elements: Vec<T> = value;
+        let num_elements = elements.len();
+        let stack_state = vec![0; num_elements];
         let stack_pointer = 1;
         let first_iter = true;
         Self {
--- src/permutations/test.rs
+++ src/permutations/test.rs
@@ -24,3 +24,28 @@ fn test_permutations() {
             .for_each(|(permutation, expected)| assert_eq!(permutation, expected.clone()))
     })
 }
+
+#[test]
+fn test_permutations_generic() {
+    let cases = [
+        (vec!['a'], vec![vec!['a']]),
+        (vec!['a', 'b'], vec![vec!['a', 'b'], vec!['b', 'a']]),
+        (
+            vec!['a', 'b', 'c'],
+            vec![
+                vec!['a', 'b', 'c'],
+                vec!['b', 'a', 'c'],
+                vec!['c', 'a', 'b'],
+                vec!['a', 'c', 'b'],
+                vec!['b', 'c', 'a'],
+                vec!['c', 'b', 'a'],
+            ],
+        ),
+    ];
+
+    cases.iter().for_each(|(n, expected)| {
+        Permutations::from(n.to_owned())
+            .zip(expected)
+            .for_each(|(permutation, expected)| assert_eq!(permutation, expected.clone()))
+    })
+}
